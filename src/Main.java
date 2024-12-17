cd576b5ce6909a81c27946bc3cc962c421647353
Xushnid <xmamtqosimov@gmail.com>
2024-12-17 22:34:29 +0900

    new changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This file is created for git in order to check repository");
+        System.out.println("to check");
     }
 }
